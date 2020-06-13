fdc0e6ce9a7cad11b34ab78573d8824c7fe4526b
Douville <Michelle.Douville@gov.bc.ca>
2020-06-12 19:49:27 -0700

    coments updated

--- teams_ad_user_sync.ps1
+++ teams_ad_user_sync.ps1
@@ -1,11 +1,12 @@
 #author: Michelle.Douville@gov.bc.ca 
 #date: June 11, 2020
-#description: The script syncs members between a specified MS Teams TEAM and an Active Directory Group. 
-#Removing members from a MS Teams TEAM is an optional flag <WithRemove>
-#<TestOnly or Update> Flag can be used to do a test run from sync to get a report of changes and a CSV lists of AD and MS Teams TEAM current members.
 
-#usage: teams_ad_user_sync.ps1 <Team Group ID> <Active Directory Group that contain Members to add or remove from MS Teams TEAM> <WithRemove flag>
-#example: c:\sw_nt\Git\msteams-helper\teams_ad_user_sync.ps1 "1f6cded9-2277-49d6-8d5c-2ec7fc9d6639" "CSNRIMIT" "WithRemove" "TestOnly"
+#description: The powershell script syncs members between a specified MS Teams TEAM and an Active Directory Group. 
+#             <WithRemove> is an optional flag for removing members from a MS Teams TEAM (ie. leave out if you only want to ADD users)
+#             <TestOnly or Update> is a flag that can be used to do a test run for a sync, to get a report of potential changes, and to create CSV lists of AD and MS Teams TEAM current members.
+
+#usage:     teams_ad_user_sync.ps1 <Team Group ID> <Active Directory Group that contain Members to add or remove from MS Teams TEAM> <WithRemove flag> <TestOnly or Update flag>
+#example:   teams_ad_user_sync.ps1 "1f6cded9-2277-49d6-8d5c-2ec7fc9d6639" "CSNRIMIT" "WithRemove" "TestOnly"
 
 #References:
 #https://github.com/microsoftgraph/msgraph-sdk-powershell/blob/dev/samples/4-UsersAndGroups.ps1
@@ -15,20 +16,23 @@
 #https://community.spiceworks.com/topic/444390-get-adgroupmembers-emails
 #https://docs.microsoft.com/en-us/powershell/module/teams/add-teamuser?view=teams-ps
 
-## DEPENDENCIES Install powershell modules via powershell prompt (this took way too long, requires admin access):
+##     **** NEEDS TO BE COMPLETED ONCE ON DEVICE **** DEPENDENCIES 
+##     Install powershell modules via powershell prompt (this took way too long, requires admin access):
 #Install-Module -Name MicrosoftTeams
 #Install-module Microsoft.Graph
 
-##This may be a one time step, to allow powershell via the device:
+##     **** NEEDS TO BE COMPLETED ONCE ON DEVICE **** 
+##     This may be a one time step, to allow powershell via the device:
 #Connect-Graph
  
-##Connect to MS Teams this is required on session startup"
+##     **** NEEDS TO BE COMPLETED ONCE ON DEVICE PER SESSION **** 
+##     Connect to MS Teams this is required on session startup - 
 #Connect-MicrosoftTeams
 
 ## HELPER COMANDS
-## Find the Team (I sniffed my web traffic to find the GroupID for the IIT All staff Team)
+##     Find the Team (I sniffed my web traffic to find the GroupID for the IIT All staff Team)
 #Get-TeamChannel -GroupId 1f6cded9-2277-49d6-8d5c-2ec7fc9d6639  
-## Lists all the users in the Teams Channel:
+##     Lists all the users in the Teams Channel:
 # Get-TeamUser -GroupId 1f6cded9-2277-49d6-8d5c-2ec7fc9d6639
 
 $TeamGroupID=$args[0]
